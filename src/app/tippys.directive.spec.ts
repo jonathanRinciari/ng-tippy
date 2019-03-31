32f7d4c4668de31557cc5530df959b587e3b31ef
Jonathan Rinciari <jrinciaritosd@gmail.com>
2019-03-30 23:46:44 -0400

    Removed temp directive

--- src/app/tippys.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { TippysDirective } from './tippys.directive';
-
-// describe('TippysDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new TippysDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
